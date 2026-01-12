8053cb0f0565b440d827198eeeeee0ea5805a0e5
MaickDavila <librajunior13@gmail.com>
2026-01-11 22:17:25 -0500

    Fix entrypoint path in Astro configuration for image service

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,7 +10,7 @@ export default defineConfig({
   integrations: [vue(), tailwind()],
   image: {
     service: {
-      entrypoint: "astro:assets/services/noop",
+      entrypoint: "astro/assets/services/noop",
     },
   },
 });
